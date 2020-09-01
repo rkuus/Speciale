34438b07ba5f810614214d7e71390aff4442c5a0
Rasmus Kuus <rakuu16@student.sdu.dk>
2020-09-01 14:47:55 +0200

    ur5 updated
    Works as the ur3, but need further addition before ai implementation

---
+++ ml-agents-release_6/Project/Assets/ML-Agents/Examples/ur5_unity-master/Assets/Scripts/jointController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jointController : MonoBehaviour
+{
+
+    public float speed = 100.0f;
+    private ArticulationBody articulation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        articulation = GetComponent<ArticulationBody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical");
+        //ArticulationReducedSpace newVelocity = new ArticulationReducedSpace(translation);
+        if (translation != 0.0f)
+        {
+            float rotationChange = (float)translation * speed * Time.fixedDeltaTime;
+            float rotationGoal = CurrentPrimaryAxisRotation() + rotationChange;
+            RotateTo(rotationGoal);
+        }
+        
+    }
+
+    public void ForceToRotation(float rotation)
+    {
+        // set target
+        RotateTo(rotation);
+
+        // force position
+        float rotationRads = Mathf.Deg2Rad * rotation;
+        ArticulationReducedSpace newPosition = new ArticulationReducedSpace(rotationRads);
+        articulation.jointPosition = newPosition;
+
+        // force velocity to zero
+        ArticulationReducedSpace newVelocity = new ArticulationReducedSpace(0.0f);
+        articulation.jointVelocity = newVelocity;
+
+    }
+
+    public float CurrentPrimaryAxisRotation()
+    {
+        float currentRotationRads = articulation.jointPosition[0];
+        float currentRotation = Mathf.Rad2Deg * currentRotationRads;
+        return currentRotation;
+    }
+
+    void RotateTo(float primaryAxisRotation)
+    {
+        var drive = articulation.xDrive;
+        drive.target = primaryAxisRotation;
+        articulation.xDrive = drive;
+    }
+}
